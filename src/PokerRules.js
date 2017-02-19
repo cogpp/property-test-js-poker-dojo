7f5bb3cb227cd57440b6002dccb099f6073ddbf6
blundj02 <jack.blundell@bbc.co.uk>
2017-02-19 15:57:05 +0000

    Tested Four Of A Kind

--- src/PokerRules.js
+++ src/PokerRules.js
@@ -3,13 +3,23 @@ var Rank = require('../src/Rank.js');
 function PokerRules() {}
 
 PokerRules.prototype.rankHand = function(hand) {
-   var sortedValues = hand.map( function(card){ return card.value }).sort(sortNumber).reverse()
-   var isStraight = this.isConsecutive(sortedValues)
+   var sortedValues = hand.map( function(card){ return card.value }).sort(sortNumber).reverse();
+   var isStraight = this.isConsecutive(sortedValues);
    var isFlush = new Set(hand.map( function(card){ return card.suit })).size == 1;
+   var freqOfValues = this.groupByFrequencyOfCardValues(sortedValues);
    if (isStraight && isFlush) {
       return Rank.STRAIGHT_FLUSH;
+   } else if (Object.values(freqOfValues).includes(4)) {
+      return Rank.FOUR_OF_A_KIND;
    }
    return -1;
+};
+
+
+PokerRules.prototype.groupByFrequencyOfCardValues = function (cardValues) {
+    var valueTally = {};
+    cardValues.forEach( function (value) { if (value in valueTally) valueTally[value] ++; else valueTally[value] = 1; } );
+    return valueTally;
 };
 
 PokerRules.prototype.isConsecutive = function (cardValues) {
@@ -25,5 +35,7 @@ PokerRules.prototype.isConsecutive = function (cardValues) {
 function sortNumber(a,b) {
     return a - b;
 }
+
+PokerRules.suits = ['D','H','C','S'];
 
 module.exports = PokerRules;
