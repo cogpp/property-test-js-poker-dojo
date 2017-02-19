f796e0777a8395b02a874a8bba9854c1cd3e7cd4
blundj02 <jack.blundell@bbc.co.uk>
2017-02-19 15:57:05 +0000

    Tested Four Of A Kind

--- src/Rank.js
+++ src/Rank.js
@@ -1,5 +1,6 @@
 function Rank() {}
 
 Rank.STRAIGHT_FLUSH = 0
+Rank.FOUR_OF_A_KIND = 1
 
 module.exports = Rank;
--- test/pokerProperties.js
+++ test/pokerProperties.js
@@ -12,16 +12,36 @@ describe("The Poker Hand Ranking should be able to recognise", function () {
         qc.forAll(topCardValueInAStraight, suitGenerator, function(topValue, suit) {
             var hand = [];
             for (var i = 0; i < 5; i++) {
-                hand.push(new Card(topValue-i,suit))
+                hand.push(new Card(topValue-i, suit))
             }
             var result = new PokerRules().rankHand(hand);
             assert(result === Rank.STRAIGHT_FLUSH);
         })
     });
 
+    it('four of a kind', function() {
+        qc.forAll(cardValueGenerator, cardValueGenerator, suitGenerator, function(value1, value2, suit) {
+            if (value1 != value2) {
+                var hand = [];
+                PokerRules.suits.forEach( function(suit){ hand.push(new Card(value1, suit)) });
+                hand.push(new Card(value2, suit));
+                var result = new PokerRules().rankHand(hand);
+                assert(result === Rank.FOUR_OF_A_KIND);
+            }
+        })
+    });
 
+
+    var cardValueGenerator = qc.pick(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14);
     var topCardValueInAStraight = qc.pick(6, 7, 8, 9, 10, 11, 12, 13, 14);//TODO range
-    var suitGenerator =  qc.pick('D','H','C','S');// //TODO qc.pick(Suit.CLUBS, Suit.HEARTS, Suit.SPADES, Suit.DIAMOND);
+    var suitGenerator =  qc.pick(PokerRules.suits);// //TODO qc.pick(Suit.CLUBS, Suit.HEARTS, Suit.SPADES, Suit.DIAMOND);
+    var fiveSuitGenerator = qc.arrayOf(suitGenerator, {length: 5});
+    // var twoDifferentCardValues = function() {
+    //     var cardValue1 = cardValueGenerator
+    //     var cardValue2 = qc.except(cardValueGenerator, cardValue1)
+    //     return [cardValue1, cardValue2]
+    // }
+
     // var cardGenerator = qc.constructor(Card, valueGenerator,  suitGenerator);
     // var fiveDistinctCardValues  = qc.array.subsetOf([2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14], {length: 5});
 });
